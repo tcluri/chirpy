df81acae65332033dc0cab98d7a30a0706a8924e
Tejaa Chintaluri <saitejaac@gmail.com>
2023-06-01 17:12:08 +0530

    A simple server

--- main.go
+++ main.go
@@ -1,8 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func middlewareCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Headers", "*")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
 	fmt.Println("Hello! Welcome to the chirpy webserver!")
 
+	mux := http.NewServeMux()
+	corsMux := middlewareCors(mux)
+
+	newServer := http.Server{
+		Addr:    "localhost:8080",
+		Handler: corsMux,
+	}
+	newServer.ListenAndServe()
 }
