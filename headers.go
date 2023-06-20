ef32656d66699eced072be1894b36944106f864d
Tejaa Chintaluri <saitejaac@gmail.com>
2023-06-20 08:06:37 +0530

    Fix cors error by setting Access-Control-Allow-Headers in the headers

--- headers.go
+++ headers.go
@@ -8,7 +8,7 @@ func middlewareCors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Headers", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
 			return
